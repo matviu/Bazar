14d20cfa4f4eac7ebc06f626a4020d6ec6078302
matviu <matvi.u@yandex.ru>
2017-09-15 16:24:21 +0500

    сверстал topline2, написал логику выпадающих меню

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 window.onload = function() {
 
-  (function openLoginWindow() {
+  (function loginPopup() {
 
     var btnLoginOpen = document.querySelector('.user-block__login');
     var btnLoginClose = document.querySelector('.login__close');
@@ -8,28 +8,69 @@ window.onload = function() {
     var cover = document.querySelector('.cover');
     var loginField = popupLogin.querySelector('[name=login]');
 
-    btnLoginOpen.onclick = function() {
-      event.preventDefault();
-      popupLogin.classList.add("login-wrapper--show");
-      cover.classList.add("cover--show");
-      loginField.focus();
+    btnLoginOpen.onclick = showPopup;
 
-    }
-
-    btnLoginClose.onclick = function() {
-      event.preventDefault();
-      popupLogin.classList.remove("login-wrapper--show");
-      cover.classList.remove("cover--show");
-    }
+    btnLoginClose.onclick = closePopup;
 
     window.addEventListener("keydown", function() {
+      console.log(event)
       if(event.keyCode == 27 && popupLogin.classList.contains("login-wrapper--show")) {
-        popupLogin.classList.remove("login-wrapper--show");
-        cover.classList.remove("cover--show");
+         closePopup();
       }
     })
 
+    function showPopup() {
+      event.preventDefault();
+      popupLogin.classList.add("login-wrapper--show")
+      cover.classList.add("cover--show");
+      loginField.focus();
+    }
+
+    function closePopup() {
+      event.preventDefault();
+      popupLogin.classList.remove("login-wrapper--show")
+      cover.classList.remove("cover--show");
+      loginField.blur();
+    }
 
   })();
+
+  (function mainMenu() {
+
+    var btnToggle = document.querySelector('.main-menu__toggle');
+    var menu = document.querySelector('.main-menu__list');
+    var menuWrapper = document.querySelector('.main-menu__list-wrapper');
+
+    btnToggle.onclick = toggleMenu;
+
+    function toggleMenu() {
+      event.preventDefault();
+      menu.classList.toggle('main-menu__list--show');
+      btnToggle.classList.toggle('main-menu__toggle--opened');
+      menuWrapper.classList.toggle('main-menu__list-wrapper--show');
+    }
+
+  })();
+
+  (function mainCart() {
+
+    var btnToggle = document.querySelector('.main-cart__toggle');
+    var cartList = document.querySelector('.main-cart__list');
+    var cartListWrapper = document.querySelector('.main-cart__list-wrapper');
+
+    btnToggle.onclick = toggleCartList;
+
+    function toggleCartList() {
+      event.preventDefault();
+      cartList.classList.toggle('main-cart__list--show');
+      btnToggle.classList.toggle('main-cart__toggle--opened');
+      cartListWrapper.classList.toggle('main-cart__list-wrapper--show');
+    }
+
+  })();
+
+
+
+
 
 }
